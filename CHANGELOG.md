bc3b5cf14de312958f516d4f431644f84d3e0a06
cf <1406211263@qq.com>
2020-05-22 09:48:33 +0800

    fix: 修复路由权限验证问题

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 1.0.0 (2020-05-21)
+# 1.0.0 (2020-05-22)
 
 范围|描述|commitId
 --|--|--
@@ -71,6 +71,7 @@
 范围|描述|commitId
 --|--|--
  - | yangshi | [ecb14ea](https://code.hzmantu.com/mantu-tech/fed/show-pic-management/commits/ecb14ea)
+ - | 优化lodash | [d0d041d](https://code.hzmantu.com/mantu-tech/fed/show-pic-management/commits/d0d041d)
  - | 更改部分字段显示错误问题 | [d2b1bb4](https://code.hzmantu.com/mantu-tech/fed/show-pic-management/commits/d2b1bb4)
  - | 自动更改更新时间 | [b5f84ce](https://code.hzmantu.com/mantu-tech/fed/show-pic-management/commits/b5f84ce)
 
--- src/router/modules/userManagement.js
+++ src/router/modules/userManagement.js
@@ -3,19 +3,19 @@ import Layout from '@/layout'
 const userManagement = {
   path: '/user-management',
   component: Layout,
-  name: 'userManagement',
+  name: 'UserManagement',
   redirect: '/user-management/user-manage',
   meta: { title: '用户管理', icon: 'iconfont iconyonghuguanli' },
   children: [
     {
       path: 'user-manage',
-      name: 'userManage',
+      name: 'UserManage',
       component: () => import('@/views/user-management/user-management.vue'),
       meta: { title: '用户管理', icon: '', breadcrumb: false }
     },
     {
       path: 'user-detail',
-      name: 'userDetail',
+      name: 'UserDetail',
       component: () => import('@/views/user-management/user-detail.vue'),
       meta: { title: '用户详情', icon: '', activeMenu: '/user-management/user-manage' },
       hidden: true
